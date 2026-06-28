88b045ab583ffa042adafe4ce7d27e55879a60ba
ArteVisualenVivo <artevisualenvivo@gmail.com>
2026-06-28 19:49:02 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: ".next-local",
   turbopack: {
     root: "C:\\Users\\Cesar\\Desktop\\operario-control",
   },
